9932efd8304da3981547c01dda5a013c912235cc
Andriy Yanko <andriy.yanko@gmail.com>
2011-11-09 15:49:46 +0200

    Added X11Client#root_window

--- lib/x11_client.rb
+++ lib/x11_client.rb
@@ -1,4 +1,9 @@
 require "x11_client_ext"
 
 class X11Client
+
+  def root_window
+    get_window(root_window_id)
+  end
+
 end
--- spec/x11_client_spec.rb
+++ spec/x11_client_spec.rb
@@ -1,11 +1,25 @@
 require "spec_helper"
 
-describe X11Client, "#root_window_id" do
+describe X11Client do
   let(:client) { described_class.new(ENV['DISPLAY']) }
 
-  it "should be root window integer" do
-    root_window_id = `xwininfo -root -int | grep "Window id:"`.scan(/\d+/).first.to_i
-    root_window_id.should_not be_zero
-    client.root_window_id.should == root_window_id
+  let(:root_window_id) { `xwininfo -root -int | grep "Window id:"`.scan(/\d+/).first.to_i }
+
+  describe "#root_window_id" do
+    it "should be root window integer" do
+      root_window_id.should_not be_zero
+      client.root_window_id.should == root_window_id
+    end
+  end
+
+  describe "#root_window" do
+    it "should return hash of root window attributes" do
+      window = client.root_window
+      window.should be_kind_of(Hash)
+      window['id'].should == root_window_id
+      window['root_id'].should == root_window_id
+      window['x'].should == 0
+      window['y'].should == 0
+    end
   end
 end
